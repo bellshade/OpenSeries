956d1787cbb7df5b17a0ec36bf9a117487e67aab
slowy07 <slowy.arfy@gmail.com>
2023-12-09 21:20:32 +0700

    menambahkan rumus fisika
    menambahkan file gitignore untuk meng ignore file __py_cache__

--- example/example_fisika.py
+++ example/example_fisika.py
@@ -1,4 +1,4 @@
-import WpuPythonSeries.fisika as fisika
+import OpenSeries.fisika as fisika
 
 print("hitung kecepatan")
 result_kecepatan = fisika.kecepatan(4, 2.3)
@@ -7,3 +7,12 @@ print(f"hasil dari kecepatan adalah: {result_kecepatan}")
 print("hitung percepatan")
 result_percepatan = fisika.percepatan(4, 2.3)
 print(f"hasil dari percepatan adalah: {result_percepatan}")
+
+print("hitung glbb (gerak lurus beraturan)")
+result_glbb = fisika.rumus_glbb(10, 2, 3)
+print(f"hasil dari glbb adalah: {result_glbb}")
+
+tambah = 1 + 3
+print("hasil adalah", tambah)
+print("hasil adalah {}".format(tambah))
+print(f"hasil adalah {tambah}")

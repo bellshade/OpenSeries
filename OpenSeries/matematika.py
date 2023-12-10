855a7e6d6faba6411725b4ba913b534ed4e1446b
slowy07 <slowy.arfy@gmail.com>
2023-12-10 12:10:22 +0700

    menambahkan fungsi dari hitung nilai ratai-rata

--- OpenSeries/matematika.py
+++ OpenSeries/matematika.py
@@ -1,4 +1,5 @@
 from OpenSeries.util import constant
+import math
 
 
 def luas_lingkaran(jari: float | int) -> float:
@@ -8,4 +9,29 @@ def luas_lingkaran(jari: float | int) -> float:
     Parameter:
     jari-jari (float atau integer): jari jari yang akan dihitung
     """
-    return constant.pi * jari ** 2
+    return constant.pi * jari**2
+
+
+def persamaan_linear(a: int | float, b: int | float, c: int | float) -> int | float:
+    """
+    menghitung persamaan linear
+
+    parameter:
+        a (float atau integer): nilai a
+        b (float atau integer): nilai b
+        c (float atau integer): nilai c
+
+    return:
+        (float atau integer): hasil persamaan linear dari 3 bilangan a, b, dan c
+    """
+    return (-b + math.sqrt(b**2 - 4 * a * c)) / 2 * a
+
+
+def rata_rata(nilai: list[int | float]) -> list[int | float]:
+    """
+    menghitung nilai rata-rata
+
+    parameter:
+        nilai (list(float atau integer)): nilai yang dihitung
+    """
+    return sum(nilai) / len(nilai)
--- example/example_matematika.py
+++ example/example_matematika.py
@@ -1,5 +1,9 @@
-import WpuPythonSeries.matematika as matematika
+import OpenSeries.matematika as matematika
 
 print("hitung jari jari lingkaran")
 hasil_jari_lingkaran = matematika.luas_lingkaran(4.5)
 print(f"hasil dari hitung jari jari lingkaran: {hasil_jari_lingkaran}")
+
+nilai_rata = [2, 3, 5, 5, 6, 1, 2, 3]
+print(f"menghitung nilai rata-rata dari {nilai_rata}")
+print(f"hasilnya adalah: {matematika.rata_rata(nilai_rata)}")

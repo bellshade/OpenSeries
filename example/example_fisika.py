c4263d3ee94c7169005c6f96c5559f3ca132c88b
slowy07 <slowy.arfy@gmail.com>
2024-01-01 21:18:37 +0700

    update project OpenSeries

--- example/example_fisika.py
+++ example/example_fisika.py
@@ -5,17 +5,17 @@ import OpenSeries.fisika as fisika
 # fungsi menghitung kecepatan
 print("hitung kecepatan")
 result_kecepatan = fisika.kecepatan(4, 2.3)
-print(f"hasil dari kecepatan adalah: {result_kecepatan}")
+print(f"hasil dari kecepatan adalah: {result_kecepatan}\n")
 
 # fungsi menghitung percepatan
 print("hitung percepatan")
 result_percepatan = fisika.percepatan(4, 2.3)
-print(f"hasil dari percepatan adalah: {result_percepatan}")
+print(f"hasil dari percepatan adalah: {result_percepatan}\n")
 
 # fungsi menghitung glbb
 print("hitung glbb (gerak lurus beraturan)")
 result_glbb = fisika.gerak_lurus_beraturan(10, 2, 3)
-print(f"hasil dari glbb adalah: {result_glbb}")
+print(f"hasil dari glbb adalah: {result_glbb}\n")
 
 # fungsi menghitung energi kinetik
 massa_benda = 14
@@ -23,11 +23,33 @@ kecepatan_benda = 23.4
 print(
     f"menghitung energi kinetik pada benda {massa_benda} kg dengan kecepatan {kecepatan_benda} m/s"
 )
-print(f"hasilnya adalah : {fisika.energi_kinetik(massa_benda, kecepatan_benda)} joule")
+print(
+    f"hasilnya adalah : {fisika.energi_kinetik(massa_benda, kecepatan_benda)} joule\n"
+)
 
 # menghitung masa jenis benda
 volume_benda = 8
 print(
     f"menghitung massa jenis benda dengan massa benda {massa_benda} gram, dan volume benda {volume_benda} cm3"
 )
-print(f"hasilnya adalah : {fisika.masa_jenis(massa_benda, volume_benda)}")
+print(f"hasilnya adalah : {fisika.masa_jenis(massa_benda, volume_benda)}\n")
+
+# menghitung energi potensial
+massa_benda_potensial = 12
+gravitasi_bumi = 9.78
+ketinggian_benda = 400
+print("menghitung energi potensial dari suatu benda")
+print(f"dengan massa benda adalah: {massa_benda_potensial}")
+print(f"gravitasi bumi {gravitasi_bumi}")
+print(f"dan ketinggian benda adalah {ketinggian_benda}")
+print(
+    f"hasilnya adalah: {fisika.energi_potensial(massa_benda_potensial, gravitasi_bumi, ketinggian_benda)}\n"
+)
+
+# menghitung hukum arus ohm
+kuat_arus = 30
+hambatan = 3
+print("menghitung hukum ohm")
+print(f"dengan kuat arus {kuat_arus}")
+print(f"hambatan {hambatan}")
+print(f"hasilnya adalah: {fisika.hukum_ohm(kuat_arus, hambatan)}\n")
--- example/example_matematika.py
+++ example/example_matematika.py
@@ -2,27 +2,32 @@ import OpenSeries.matematika as matematika
 
 print("hitung jari jari lingkaran")
 hasil_jari_lingkaran = matematika.luas_lingkaran(4.5)
-print(f"hasil dari hitung jari jari lingkaran: {hasil_jari_lingkaran}")
+print(f"hasil dari hitung jari jari lingkaran: {hasil_jari_lingkaran}\n")
 
 nilai_rata = [2, 3, 5, 5, 6, 1, 2, 3]
 print(f"menghitung nilai rata-rata dari {nilai_rata}")
-print(f"hasilnya adalah: {matematika.rata_rata(nilai_rata)}")
+print(f"hasilnya adalah: {matematika.rata_rata(nilai_rata)}\n")
 
 nilai_faktorial = 25
 print(f"menghitung nilai faktorial dari angka {nilai_faktorial}")
-print(f"hasilnya adalah : {matematika.faktorial(nilai_faktorial)}")
+print(f"hasilnya adalah : {matematika.faktorial(nilai_faktorial)}\n")
 
 nilai_permutasi = 12
 print(f"menghitung nilai permutasi dari {nilai_permutasi}")
-print(f"hasilnya adalah : {matematika.permutasi(nilai_permutasi, 2)}")
+print(f"hasilnya adalah : {matematika.permutasi(nilai_permutasi, 2)}\n")
 
 nilai_kombinasi = 15
 print(f"menghitung nilai kombinasi dari {nilai_kombinasi}")
-print(f"hasilnya adalah : {matematika.kombinasi(nilai_kombinasi, 3)}")
+print(f"hasilnya adalah : {matematika.kombinasi(nilai_kombinasi, 3)}\n")
 
 nilai1 = 24
 nilai2 = 40
 print(
     f"hitung nilai fpb (faktor persekutuan terbesar) dari nilai {nilai1} dan {nilai2}"
 )
-print(f"hasilnya adalah : {matematika.fpb(nilai1, nilai2)}")
+print(f"hasilnya adalah : {matematika.fpb(nilai1, nilai2)}\n")
+
+rentang_nilai = 300
+print(
+    f"rentang nilai {rentang_nilai} faktor prima adalah {matematika.faktor_prima(rentang_nilai)}\n"
+)

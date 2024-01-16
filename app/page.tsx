7bdd7a161e26490294ff4838e78f36f9d4c8b1c6
envilion s. <pow.zone@outlook.com>
2024-01-16 13:06:21 +0700

    feat: setup mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <p className='text-2xl font-bold text-zinc-800 underline'>
+        <p className="text-2xl font-bold text-zinc-800 underline">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, libero amet ex tenetur placeat
             doloremque ullam commodi vel, ducimus fuga, quibusdam illo maiores possimus est. Quisquam assumenda saepe
             odit accusantium.

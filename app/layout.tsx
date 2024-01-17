33e074a27f611ab365460a020184cae919ddd27f
envilion s. <pow.zone@outlook.com>
2024-01-17 10:08:30 +0700

    add: progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${inter.className} bg-zinc-200`}>{children}</body>
+            <body className={`${inter.className} bg-zinc-200`}>
+                <NextTopLoader color="#27272a" />
+                {children}
+            </body>
         </html>
     );
 }

fb0566920462eb7f01823e182118ad9fee7e4b0f
envilion s. <pow.zone@outlook.com>
2024-02-03 19:56:57 +0700

    feat: new syntax highlighting

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,12 +1,14 @@
+"use client";
+
 import Sidebar from "@/components/Sidebar";
 
-export default async function MdxLayout({ children }: { children: React.ReactNode }) {
+export default function MdxLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <div className="flex h-dvh max-h-dvh flex-col overflow-hidden">
                 <div className="relative flex h-full">
                     <Sidebar />
-                    <main className="prose prose-indigo w-full max-w-none overflow-y-auto rounded-xl p-10 prose-h1:bg-gradient-to-br prose-h1:from-indigo-600 prose-h1:to-rose-400 prose-h1:bg-clip-text prose-h1:text-transparent prose-img:w-full prose-img:rounded-xl">
+                    <main className="prose prose-indigo w-full max-w-none overflow-y-auto rounded-xl p-10 prose-h1:bg-gradient-to-br prose-h1:from-indigo-600 prose-h1:to-rose-400 prose-h1:bg-clip-text prose-h1:text-transparent prose-pre:border prose-img:w-full prose-img:rounded-xl">
                         {children}
                     </main>
                 </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,23 @@
 import remarkGfm from "remark-gfm";
 import createMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ["js", "jsx", "mdx", "md", "ts", "tsx"]
 };
 
+const options = {
+    theme: {
+        light: "light-plus",
+        dark: "dark-plus"
+    }
+};
+
 const withMDX = createMDX({
     options: {
         remarkPlugins: [remarkGfm],
-        rehypePlugins: []
+        rehypePlugins: [[rehypePrettyCode, options]]
     }
 });
 

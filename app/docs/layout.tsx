46181e339ac0abec53ac160e408e6c356d424cbf
envilion s. <pow.zone@outlook.com>
2024-02-04 19:01:35 +0700

    feat: create custom runtime

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,20 +3,24 @@
 import Sidebar from "@/components/Sidebar";
 import { useNavStyle } from "@/hooks/useNavStyle";
 import { useEffect } from "react";
+import { PythonProvider } from "react-py";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     const { setDocsStyle } = useNavStyle();
+    const packages = {
+        micropip: ["OpenSeriesBellshade"]
+    };
 
     useEffect(() => {
         setDocsStyle();
     }, [setDocsStyle]);
 
     return (
-        <>
+        <PythonProvider packages={packages}>
             <Sidebar />
             <main className="prose prose-indigo max-w-none overflow-y-auto rounded-xl p-8 dark:prose-invert prose-pre:border prose-img:w-full prose-img:rounded-xl dark:prose-h1:bg-clip-text dark:prose-h1:font-bold dark:prose-pre:border-zinc-700 lg:pl-80">
                 {children}
             </main>
-        </>
+        </PythonProvider>
     );
 }

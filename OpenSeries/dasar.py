e05dabfb734fc29dff0a584386c9f67fda98ddf2
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2024-02-16 00:08:02 +0700

    feat: menambahkan algorithma akar (#62)
    * feat: menambahkan algorithma akar
    
    * feat: menambahkan algorithma akar
    
    * feat: menambahkan algorithma akar
    
    * docs: update docstring information
    
    ---------
    
    Co-authored-by: arfy slowy <slowy.arfy@gmail.com>

--- OpenSeries/dasar.py
+++ OpenSeries/dasar.py
@@ -23,3 +23,29 @@ def bulat(
         return int(angka) if angka - int(angka) >= 0 else int(angka) - 1
     else:
         return math.ceil(angka)
+
+
+def akar(
+    value: Union[int, float], iterasi: int = 4
+) -> Union[float, error.ErrorTipeData]:
+    """
+    apromasi nilai akar pada angka
+    Args:
+        value (Union[int,float]): input nilai
+        iterasi (Optional[int]): mengsetup iterasi apporamasi. Defaults to 4.
+
+    Returns:
+        float: output dari angka yang telah di prediksi
+        error.ErrorTipeData: error jika data yang dimasukkan salah
+    """
+
+    # ngecheck tipe data pada value
+    if not isinstance(value, (int, float)):
+        raise error.ErrorTipeData(["int", "float"])
+    # ngecheck tipe data pada iterasi
+    if not isinstance(iterasi, int):
+        raise error.ErrorTipeData(["int"])
+    result = value
+    for _ in range(iterasi + 1):
+        result = (result + (value / result)) / 2
+    return round(result, 2)
--- testing/dasar.py
+++ testing/dasar.py
@@ -19,3 +19,30 @@ class TestBulat(unittest.TestCase):
     def test_error_tipe_data(self):
         with self.assertRaises(error.ErrorTipeData):
             raise opseries.bulat("2.5")
+
+
+class TestAkar(unittest.TestCase):
+    def setUp(self):
+        self.angka = 4
+        self.iterasi = 4
+
+    def test_akar_integer(self):
+        self.assertEqual(opseries.akar(self.angka), 2)
+
+    def test_akar_floar(self):
+        self.assertEqual(opseries.akar(float(self.angka)), 2.0)
+
+    def test_akar_string(self):
+        with self.assertRaises(error.ErrorTipeData):
+            raise opseries.akar(str(self.angka))
+
+    def test_akar_iterasi_int(self):
+        self.assertEqual(opseries.akar(self.angka, iterasi=self.iterasi), 2)
+
+    def test_akar_iterasi_float(self):
+        with self.assertRaises(error.ErrorTipeData):
+            raise opseries.akar(self.angka, float(self.iterasi))
+
+    def test_akar_iterasi_string(self):
+        with self.assertRaises(error.ErrorTipeData):
+            raise opseries.akar(self.angka, str(self.iterasi))
